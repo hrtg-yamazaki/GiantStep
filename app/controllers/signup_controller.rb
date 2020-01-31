4f2c5944ae91f02c75b8d9013247c4bc331029c2
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-01-31 17:50:44 +0900

    ユーザー登録機能のウィザード形式化
    ・ユーザーの新規登録をカラム別に２ページに分割して行う形式に変更し
    ・仮のビューとフォームを作成し、動作確認を行いました。

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,5 +1,6 @@
 class SignupController < ApplicationController
 
+
   def index
     @user = User.new
   end
@@ -8,14 +9,12 @@ class SignupController < ApplicationController
   def keep
 
     @user = User.new(
-      nickname: user_params[:nickname],
       email: user_params[:email],
       password: user_params[:password],
       password_confirmation: user_params[:password_confirmation]
     )
     
     if @user.valid?(:keep)
-      session[:nickname] = user_params[:nickname]
       session[:email] = user_params[:email]
       session[:password] = user_params[:password]
       session[:password_confirmation] = user_params[:password_confirmation]
@@ -28,21 +27,58 @@ class SignupController < ApplicationController
 
 
   def profile
-    
+    @user = User.new
   end
 
+
   def user_create
+
+    @user = User.new(
+      email: session[:email],
+      password: session[:password],
+      password_confirmation: session[:password_confirmation],
+      nickname: user_params[:nickname],
+      gender: user_params[:gender],
+      birthday: convert_params_into_date,
+      profile: user_params[:profile],
+    )
+    if @user.valid?(:user_create)
+      binding.pry
+      if @user.save
+        session[:id] = @user.id
+        sign_in User.find(session[:id]) unless user_signed_in?
+        redirect_to complete_signup_index_path
+      else
+        render "signup/profile"
+      end
+    else
+      render "signup/profile"
+    end
+
   end
+
 
   def complete
 
   end
 
   private
+
   def user_params
     params.require(:user).permit(
-      :email, :password, :password_confirmation, :nickname
+      :email, :password, :password_confirmation,
+      :nickname, :gender, :profile, :birthday
     )
+  end
+
+  def convert_params_into_date
+    year  = params[:birthday]["birthday(1i)"].to_i
+    month = params[:birthday]["birthday(2i)"].to_i
+    day   = params[:birthday]["birthday(3i)"].to_i
+    
+    if year != 0 && month != 0 && day != 0
+      return Date.new(year, month, day)
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,13 @@ class User < ApplicationRecord
   VALID_EMAIL_REGIX    = /\A[^@\s]+@[^@\s]+\z/
   VALID_PASSWORD_REGIX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{7,128}+\z/i
   
-  validates :nickname, presence: { message: "ニックネーム を入力してください" }, on: :keep, length: { maximum: 20, message: "20文字以下で入力してください" }
   validates :email,    presence: { message: "メールアドレス を入力してください" }, on: :keep, format: { with: VALID_EMAIL_REGIX,   message: "フォーマットが不適切です"}
   validates :password, presence: { message: "パスワード を入力してください" }, on: :keep, length: { maximum: 128, message: "128文字以下で入力してください" }, format: { with: VALID_PASSWORD_REGIX, message: "英字と数字両方を含むパスワードを入力してください" }, confirmation: { message: 'パスワードが一致しません'}
   validates :password_confirmation, presence: { message: '確認用にパスワードを入力してください' }, on: :keep
+
+  validates :nickname, presence: { message: "ニックネーム を入力してください" }, on: :user_create, length: { maximum: 20, message: "20文字以下で入力してください" }
+  validates :birthday, presence: { message: "生年月日 を入力してください" }, on: :user_create
+  validates :profile, length: { maximum: 1000, message: "1000文字以下で入力してください" }, on: :user_create
 
   enum gender: [ :not_public, :male, :female ]
 
