3dcb372e319fa0236e22469aef4d0115184192e1
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-02-09 18:06:05 +0900

    累計実績合計ポイントの表示
    ・Statusモデルに科目別ポイントを足し合わせるメソッドを定義し、ビューで総合ポイントを表示できるようにしました。
    ・マイページの一部レイアウトを修正しました。

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,4 +1,10 @@
 class Status < ApplicationRecord
 
   belongs_to :user
+
+  def self.total_point(status)
+    total = status.sleep + status.study + status.exercise + status.diet + status.habit + status.aim + status.login
+    return total
+  end
+
 end
