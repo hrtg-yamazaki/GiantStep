33f0d73d86d3adaf2e988cb720551ffb47ff8bee
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-02-14 16:42:58 +0900

    ユーザー関連アクセスコントロール
    ・devise関連で使用していないルーティングとビューをアプリケーションから除外しました。
    ・ユーザー詳細ページにアクセスした時、それがアクセス者自身のページだった場合は、マイページTOPにリダイレクトするよう設定しました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,9 @@ class UsersController < ApplicationController
   def show
 
     @user = User.find(params[:id])
+    if @user = current_user
+      redirect_to mypage_index_path
+    end
     @status = @user.status
 
   end
