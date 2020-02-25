9d8fb806cd2471debbac195e3498a122c8250955
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-02-25 17:11:08 +0900

    Userモデルのテスト③
    ・Userモデルのマイページにおける情報編集時に関して、単体テストを行いました。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -66,13 +66,13 @@ RSpec.describe User, type: :model do
         expect(user.errors[:password]).to include("128文字以下で入力してください")
       end
 
-      it "passwordが数字のみでは登録できない" do
+      it "passwordが数字のみでは保存できない" do
         user = build(:user, password: "1234567890")
         user.valid?(:keep)
         expect(user.errors[:password]).to include("英字と数字両方を含むパスワードを入力してください")
       end
 
-      it "passwordが英字のみでは登録できない" do
+      it "passwordが英字のみでは保存できない" do
         user = build(:user, password: "abcdefghij")
         user.valid?(:keep)
         expect(user.errors[:password]).to include("英字と数字両方を含むパスワードを入力してください")
@@ -123,7 +123,7 @@ RSpec.describe User, type: :model do
 
     end
 
-    context "保存できない" do
+    context "登録できない" do
 
       it "nicknameが空だと登録できない" do
         user = build(:user, nickname: "")
@@ -157,6 +157,77 @@ RSpec.describe User, type: :model do
 
     end
 
+  end
+
+  describe "mypage#profile_update" do
+
+    context "ユーザー情報の編集ができる" do
+
+      it "全てのカラムが埋まっていれば編集できる" do
+        user = build(:user)
+        user.valid?(:profile_update)
+        expect(user).to be_valid
+      end
+
+      it "nicknameが20文字以下であれば編集できる" do
+        user = build(:user, nickname: '12345678901234567890')
+        user.valid?(:profile_update)
+        expect(user).to be_valid
+      end
+
+      it "privacyがon_privateでも編集できる" do
+        user = build(:user, privacy: "on_private")
+        user.valid?(:profile_update)
+        expect(user).to be_valid
+      end
+
+      it "iconが空でも編集できる" do
+        user = build(:user, icon: nil)
+        user.valid?(:profile_update)
+        expect(user).to be_valid
+      end
+
+      it "profileが空でも編集できる" do
+        user = build(:user, profile: "")
+        user.valid?(:profile_update)
+        expect(user).to be_valid
+      end
+
+      it "profileが1000文字以内なら編集できる" do
+        user = build(:user, profile: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+        user.valid?(:profile_update)
+        expect(user).to be_valid
+      end
+
+    end
+
+    context "ユーザー情報の編集ができない" do
+
+      it "nicknameが空だと編集できない" do
+        user = build(:user, nickname: "")
+        user.valid?(:profile_update)
+        expect(user.errors[:nickname]).to include("ニックネーム を入力してください")
+      end
+
+      it "nicknameが21文字以上だと編集できない" do
+        user = build(:user, nickname: '123456789012345678901')
+        user.valid?(:profile_update)
+        expect(user.errors[:nickname]).to include("20文字以下で入力してください")
+      end
+
+      it "privacyが空では編集できない" do
+        user = build(:user, privacy: "")
+        user.valid?(:profile_update)
+        expect(user.errors[:privacy]).to include("公開設定が選択されていません")
+      end
+
+      it "profileが1001文字以上では編集できない" do
+        user = build(:user, profile: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab")
+        user.valid?(:profile_update)
+        expect(user.errors[:profile]).to include("1000文字以下で入力してください")
+      end
+
+    end
   end
 
 end
